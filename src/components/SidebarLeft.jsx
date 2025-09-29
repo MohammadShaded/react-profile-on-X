ddf8f508fad9746bb1fce3f5a8d698fbc7936b31
MohammadShadid <mohammadshadid03@gmail.com>
2025-09-29 17:56:27 +0300

    Build Left Sidebar user profile minimal and finish all the Left Sidebar

--- src/components/SidebarLeft.jsx
+++ src/components/SidebarLeft.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import NavBar from './NavBar';
 import TweetButton from './Common/TweetButton';
-import FollowButton from './Common/FollowButton';
+import UserProfileMinimal from './UserProfileMinimal';
+import avatarImg from '../assets/avatar.jpg';
+
 const SidebarLeft = ({ navigationItems }) => {
     return (
         <aside className="sidebar-left">
             <NavBar navigationItems={navigationItems} />
             <TweetButton onClick={() => alert('Compose new tweet')} />
+            <UserProfileMinimal
+                avatarUrl={avatarImg}
+                name="Mohammad Dwikat"
+                handle="MohammadDwikat3"
+            />
         </aside>
     );
 };
