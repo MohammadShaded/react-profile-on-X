7ce6f1e1f621c74bca21811a71b989a255863c61
MohammadShadid <mohammadshadid03@gmail.com>
2025-10-02 20:26:45 +0300

    Refactor: delte style prop and keep className only in Button.jsx

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 
-const Button = ({ children, onClick={}, className = '', style = {} }) => {
+const Button = ({ children, onClick = () => {}, className = '' }) => {
   const classes = `btn ${className}`.trim();
   return (
-    <button onClick={onClick} className={classes} style={style}>
+    <button onClick={onClick} className={classes}>
       {children}
     </button>
   );
