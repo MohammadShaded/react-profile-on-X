f7b1197933187b9d6c48f6e7cef0b78df47c61fc
MohammadShadid <mohammadshadid03@gmail.com>
2025-10-02 20:06:57 +0300

    Style: replace inline style with class in BellWithPlus.jsx puted in IconsStyles.css

--- src/components/Common/BellWithPlus.jsx
+++ src/components/Common/BellWithPlus.jsx
@@ -1,18 +1,11 @@
 import {  FaPlus,FaRegBell } from "react-icons/fa";
-
+// import ''
 const BellWithPlus = ({className}) => {
   return (
-    <div style={{ position: "relative", display: "inline-block" }}>
+    <div className="bell-plus-root" style={{ position: "relative", display: "inline-block" }}>
       <FaRegBell className={className} />
       <FaPlus 
         className={className+' plus-icon'}
-        style={{ 
-          position: "absolute", 
-          top: "2px", 
-          right: "2px", 
-          background: "black",
-          borderRadius: "50%" 
-        }} 
       />
     </div>
   );
